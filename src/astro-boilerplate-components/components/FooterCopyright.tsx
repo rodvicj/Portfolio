6e7640094c86ac55039e99463a6408839d4f130d
rodvicj <rodvicj@gmail.com>
2023-03-19 23:51:40 +0800

    refactoring

--- src/astro-boilerplate-components/components/FooterCopyright.tsx
+++ src/astro-boilerplate-components/components/FooterCopyright.tsx
@@ -1,22 +1,13 @@
-type IFooterCopyrightProps = {
-  site_name: string;
-};
-
-const FooterCopyright = (props: IFooterCopyrightProps) => (
+const FooterCopyright = () => (
   <div className="border-t border-gray-600 pt-5">
-    <div className="text-sm text-gray-200">
-      © Copyright {new Date().getFullYear()} by {props.site_name}. Built with ♥
-      by{' '}
-      <a
-        className="text-cyan-400 hover:underline"
-        href="https://creativedesignsguru.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        CreativeDesignsGuru
-      </a>
-      .
-    </div>
+    <a
+      className="text-cyan-400 hover:underline"
+      href="https://www.github.com/rodvicj"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      © rodvicj
+    </a>
   </div>
 );
 
--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,10 +1,8 @@
-import { FooterCopyright, Section } from '@/astro-boilerplate-components/index';
-
-import { AppConfig } from '@/utils/AppConfig';
+import { FooterCopyright, Section } from "@/astro-boilerplate-components/index";
 
 const Footer = () => (
   <Section>
-    <FooterCopyright site_name={AppConfig.site_name} />
+    <FooterCopyright />
   </Section>
 );
 
