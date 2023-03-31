e2f1bb8e94c3929d97fe6b36e423983fb574bcfd
rodvicj <rodvicj@gmail.com>
2023-03-31 16:42:27 +0800

    change code style

--- src/astro-boilerplate-components/components/Technology.tsx
+++ src/astro-boilerplate-components/components/Technology.tsx
@@ -4,17 +4,40 @@ type TechStackProps = {
   tags: string[];
 };
 
-const Technology = (props: TechStackProps) => (
-  <>
-    {props.tags.map(
-      (tag: string) =>
-        (
-          <Tags key={tag} color={`${ColorTags[tag.toUpperCase()]}`}>
-            {tag}
-          </Tags>
-        ) ?? <></>
-    )}
-  </>
-);
+// const Technology = (props: TechStackProps) => (
+//   <>
+//     {props.tags.map(
+//       (tag: string) =>
+//         (
+//           <Tags key={tag} color={`${ColorTags[tag.toUpperCase()]}`}>
+//             {tag}
+//           </Tags>
+//         ) ?? <></>
+//     )}
+//   </>
+// );
+
+// export { Technology };
+
+const Technology = (props: TechStackProps) => {
+  //   return (
+  //     <div>
+  //       <p>hello world!</p>
+  //     </div>
+  //   );
+  // };
+  return (
+    <>
+      {props.tags.map(
+        (tag: string) =>
+          (
+            <Tags key={tag} color={`${ColorTags[tag.toUpperCase()]}`}>
+              {tag}
+            </Tags>
+          ) ?? <></>
+      )}
+    </>
+  );
+};
 
 export { Technology };
