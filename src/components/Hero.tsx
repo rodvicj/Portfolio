4dcf4370818a8c072e0dd8f5a3fe63f4989e2ad0
rodvicj <rodvicj@gmail.com>
2023-10-05 15:22:06 +0800

    chore: simplify hero notes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,8 +17,8 @@ const Hero = () => (
         <>
           A <a className="hover:none text-cyan-400">web developer</a> from
           Philippines, welcome to my{' '}
-          <a className="hover:none text-cyan-400">portfolio</a> website, the
-          place where I showcase some of my projects.{' '}
+          <a className="hover:none text-cyan-400">portfolio</a> website where I
+          show some of my projects.{' '}
         </>
       }
       avatar={
