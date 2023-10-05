2df8465216e61f456c990bc16fa9090bb80e350a
rodvicj <rodvicj@gmail.com>
2023-10-05 09:42:11 +0800

    chore: rename from RecentPosts to AllPosts

---
+++ src/components/AllPosts.tsx
@@ -0,0 +1,32 @@
+import type { MarkdownInstance } from 'astro';
+
+import type { IFrontmatter } from '@/astro-boilerplate-components/index';
+import {
+  BlogGallery,
+  GradientText,
+  Section,
+} from '@/astro-boilerplate-components/index';
+
+type IAllPostsProps = {
+  postList: MarkdownInstance<IFrontmatter>[];
+};
+
+const AllPosts = (props: IAllPostsProps) => (
+  <Section
+    title={
+      <div className="flex items-baseline justify-between">
+        <div>
+          Recent <GradientText>Projects</GradientText>
+        </div>
+
+        <div className="text-sm">
+          <a href="/projects">View all Projects →</a>
+        </div>
+      </div>
+    }
+  >
+    <BlogGallery postList={props.postList} />
+  </Section>
+);
+
+export { AllPosts };
