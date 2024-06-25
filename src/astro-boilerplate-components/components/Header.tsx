b657558a4c9abe1e1d0f4e88fdc1368decdcfe90
rodvicj <rodvicj@gmail.com>
2024-06-25 17:08:01 +0800

    refactor: fix alignment of nav header

--- src/astro-boilerplate-components/components/Header.tsx
+++ src/astro-boilerplate-components/components/Header.tsx
@@ -1,6 +1,10 @@
 export const Header = [
-  { name: "Home", link: "/" },
-  { name: "Projects", link: "/projects" },
-  { name: "Github", link: "http://www.github.com/rodvicj" },
-  { name: "LinkedIn", link: "https://www.linkedin.com/in/rodvicj" },
+  { name: "home", link: "/" },
+  { name: "projects", link: "/projects" },
+  { name: "github", link: "http://www.github.com/rodvicj", target: "_blank" },
+  {
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/rodvicj",
+    target: "_blank",
+  },
 ];
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
   return (
     <Section>
-      <div className="flex items-center justify-between px-7 py-4 md:px-10">
+      <div className="flex items-center justify-between py-4">
         <div
           onClick={() => setOpen(!open)}
           className="absolute right-9 top-11 cursor-pointer text-3xl text-gray-200 md:hidden"
@@ -28,7 +28,7 @@ const Navbar = () => {
         </div>
 
         <ul
-          className={`transition-{text-gray-200 ease-in} absolute left-0 w-full pl-16 pt-12 duration-500 md:static md:z-auto  md:flex md:w-auto md:items-center md:bg-slate-900 md:pb-0 md:pl-0 md:transition-none ${
+          className={`transition-{text-gray-200 ease-in} absolute left-0 w-full pl-16 pt-1 duration-500 md:static md:z-auto  md:flex md:w-auto md:items-center md:bg-slate-900 md:pb-0 md:pl-0 md:transition-none ${
             open ? "top-0 bg-slate-800" : "top-[-490px]"
           }`}
         >
@@ -39,29 +39,20 @@ const Navbar = () => {
             {open ? <FontAwesomeIcon icon={faXmark} /> : <></>}
           </li>
           {Header.map((header) => (
-            <li key={header.name} className=" my-5 text-xl md:my-0 md:ml-8">
-              {header.name === "Github" || header.name === "LinkedIn" ? (
-                <GradientText>
-                  <a
-                    href={header.link}
-                    className="duration-500 hover:text-white"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {header.name}
-                  </a>
-                </GradientText>
-              ) : (
-                <GradientText>
-                  <a
-                    onClick={() => setOpen(false)}
-                    href={header.link}
-                    className="hover:text-white"
-                  >
-                    {header.name}
-                  </a>
-                </GradientText>
-              )}
+            <li
+              key={header.name}
+              className=" my-5 text-xl capitalize md:my-0 md:mr-8"
+            >
+              <GradientText>
+                <a
+                  href={header.link}
+                  className="duration-500 hover:text-white"
+                  target={header.target}
+                  rel="noopener noreferrer"
+                >
+                  {header.name}
+                </a>
+              </GradientText>
             </li>
           ))}
         </ul>
@@ -71,23 +62,3 @@ const Navbar = () => {
 };
 
 export { Navbar };
-
-// {/* <NavbarTwoColumns> */}
-// {/*   <a href="/"> */}
-// {/*   </a> */}
-// {/*   <NavMenu> */}
-// {/*     <NavMenuItem href="/projects">Projects</NavMenuItem> */}
-// {/*     <NavMenuItem href="/">GitHub</NavMenuItem> */}
-// {/*     <NavMenuItem href="/about">About</NavMenuItem> */}
-// {/*     <NavMenuItem href="/posts/post-1">post-1</NavMenuItem> */}
-// {/*   </NavMenu> */}
-// {/* </NavbarTwoColumns> */}
-//
-// {open ? (
-//   <>
-//     <FontAwesomeIcon icon={faXmark} />
-//   </>
-// ) : (
-//   <></>
-// )}
-// DeprecationWarning: Invalid 'main' field in '/home/xusr/Documents/Portfolio/node_modules/react-icons/package.json' of 'lib'.
